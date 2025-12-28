cd871e0476243d21bb343ed2da6d5d7b8aadec47
Brinkmann <obrinkmann@gmail.com>
2025-12-29 10:44:05 +1300

    Optimize homepage loading paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://www.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://firebase.googleapis.com" crossOrigin="anonymous" />
+        <link rel="prefetch" href="/catalog" as="document" />
+        <link rel="prefetch" href="/planner" as="document" />
+      </head>
       <body className="bg-background text-foreground min-h-screen flex flex-col">
         <EntitlementProvider>
           <ExerciseTypeProvider>
